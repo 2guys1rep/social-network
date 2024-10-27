a96443bff0d1803bffeeb792c74e4fc7303c024c
Rustam.Home <ka.po.po@yandex.ru>
2024-10-27 12:31:23 +0500

    Создание класса настроек

---
+++ backend/src/core/config.py
@@ -0,0 +1,26 @@
+from datetime import timedelta, datetime
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "SocialNetwork"
+    PROJECT_VERSION: str = "1.0.0"
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_EXPIRATION_TIME: timedelta = timedelta(minutes=480)
+
+    @property
+    def DB_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    model_config = SettingsConfigDict(env_file="../.env")
+
+settings = Settings()
