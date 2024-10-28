870c38a3d5295b7e7302039d89058e8066f63a28
Rustam.Home <ka.po.po@yandex.ru>
2024-10-28 08:44:00 +0500

    Добавление эндпоинтов для пользователя

--- backend/src/apis/base.py
+++ backend/src/apis/base.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from src.apis.auth.router import router as auth_router
+from src.apis.user.router import router as user_router
 
 api_router = APIRouter()
 api_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
+api_router.include_router(user_router, prefix="/user", tags=["User"])
